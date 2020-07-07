3a62f133059f70a3183e9b736d3de9b8e98d14ea
Parviz Khavari <parviz.khavari@jetbrains.com>
2020-07-07 11:10:08 +0300

    Update global variables

--- R/info.R
+++ R/info.R
@@ -4,17 +4,19 @@
 # Created on: 06.07.2020
 
 #' Hold number of infos requested
-list_infos <- 0
+pkg.env <- new.env()
+
+pkg.env$infos <- 0
 
 #' Print package info
 #' @export
 info <- function() {
-  list_infos <<- list_infos + 1
+  pkg.env$infos <- pkg.env$infos + 1
   cat("Zelenium 0.0.1 version\n")
 }
 
 #' Print number of info requests
 #' @export
 check <- function() {
-  cat("Number of infos: ", list_infos, "\n")
+  cat("Number of infos: ", pkg.env$infos, "\n")
 }
